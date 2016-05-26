2c6d407e2e38b65a000a23ac83435ab2194296d0
Saralis Rivera <email": null>
2016-05-25 20:52:34 -0400

    Fix forms but does not create new comment/review objects

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new(params[:comment])
+    @review_id = params[:review_id]
+  end
+
+  def create
+    @film = Film.find(params[:id])
+    @comment = Comment.new(params[:body])
+
+    if @comment.save
+      redirect_to film_path
+    else
+      render template: '/comments/new'
+    end
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,16 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @film = Film.find(params[:id])
+    @review = Review.new
+  end
+
+  def create
+  end
+
+  def show
+    @review = Review.find(params[:id])
+    @comments = Comment.where( comment_id: @comment.id )
+  end
+
 end
