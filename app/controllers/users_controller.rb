c18fe38fb6a42019b982cbd16a84356840ad42a4
Evan Druce & Elan Kvitko & Lisa Buch & Saralis Rivera <pair+drucee.elan71592.lisabee224.sarariv@devbootcamp.com>
2016-05-25 20:05:36 -0400

    add functionality to generate tokens and make judge users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  include UsersHelper
   def new
   end
 
@@ -25,8 +25,36 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def secret
+  end
+
+  def new_judge
+    if check_tokens( params[:secret][:secret_key] )
+      @token = params[:secret][:secret_key]
+      render template: '/users/new_judge'
+    else
+      @error = 'Your token is either no longer valid or incorrect!'
+      render template: '/users/secret'
+    end
+  end
+
+  def create_judge
+    token = params[ :user ][ :judge_token ]
+    if check_tokens( token )
+      @user = User.new( user_params )
+      if @user.save
+        @user.update_attributes( judge: true )
+        update_token = Token.find_by( token: token )
+        update_token.update_attributes( active: false )
+        session[ :user_id ] = @user.id
+        redirect_to root_path
+      end
+    else
+    end
+  end
+
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :judge, :organizer)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :judge, :judge_token, :organizer)
   end
 end
--- db/migrate/20160525162308_create_tokens.rb
+++ db/migrate/20160525162308_create_tokens.rb
@@ -1,7 +1,8 @@
 class CreateTokens < ActiveRecord::Migration
   def change
     create_table :tokens do |t|
-      t.integer :token
+      t.string :token, null: false, index: true
+      t.boolean :active, null: false, default: true
 
       t.timestamps null: false
     end
